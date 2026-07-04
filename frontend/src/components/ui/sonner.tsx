b5b8d6925e175d2c056865c9b8e2cffb81d3067d
DARLING14 <68486331+DARLING14@users.noreply.github.com>
2026-07-04 23:06:11 +0300

    Update sonner.tsx

--- frontend/src/components/ui/sonner.tsx
+++ frontend/src/components/ui/sonner.tsx
@@ -1,6 +1,7 @@
-import { Toaster as Sonner } from "sonner"
+import * as React from "react";
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
@@ -11,7 +12,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
         classNames: {
           toast:
             "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
+          description:
+            "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
@@ -20,7 +22,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
